828ff531005ee59538035507766dafce093186e9
ZenkiOo <zenky@list.ru>
2022-11-14 13:39:05 +0300

    add seats style

--- src/components/Coaches/Coach.jsx
+++ src/components/Coaches/Coach.jsx
@@ -4,11 +4,13 @@ import Coupe from './Coupe';
 export default function Coach({ coach, map }) {
   const coupesList = map.map((coupe, i) => {
     // console.log(coupe);
-    return <Coupe key={i} seats={coupe} type={coach.coach.class_type}/>;
+    return <Coupe key={i} seats={coupe} type={coach.coach.class_type} />;
   });
   return (
     <>
-      <div>{coach.coach.class_type}</div>
+      <div style={{ marginBottom: 40 + 'px'}}>
+        {coach.coach.class_type}
+      </div>
       <div className={`rly_coach rly_coach--${coach.coach.class_type}`}>
         {coupesList}
       </div>
